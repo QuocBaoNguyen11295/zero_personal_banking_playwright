7875384f61a65789aea0b0dd5e6ab4659dfec7f5
QuocBaoNguyen11295 <nqb11295@gmail.com>
2022-10-26 00:56:49 +0700

    Account Activity - Find Transation

--- helpers/onlineBankingHelpers/accountActivityHelper.ts
+++ helpers/onlineBankingHelpers/accountActivityHelper.ts
@@ -10,3 +10,22 @@ export async function verifyDataInTransaction(page) {
     const list_element = await page.$$('#all_transactions_for_account > table > tbody > tr')
     expect(list_element.length).toEqual(3)
 }
+
+export async function clickOnFindTransaction(page) {
+    await page.click('#tabs > ul > li:nth-child(2)')
+}
+
+export async function verifyDataInFindTransaction(page) {
+    await page.type('#aa_description','ONLINE TRANSFER REF #UKKSDRQG6L')
+    await page.type('#aa_fromDate','2012-09-01')
+    await page.type('#aa_toDate','2012-09-10')
+    await page.type('#aa_fromAmount','1')
+    await page.type('#aa_toAmount','1000')
+    await page.selectOption('#aa_type','DEPOSIT')
+    await page.click('button[type="submit"]')
+    await page.locator('#filtered_transactions_for_account > table').isVisible()
+    const elements = await page.$$('#filtered_transactions_for_account > table > tbody > tr')
+    for(var i=1;i <= elements.length;i++){
+        await expect(page.locator('#filtered_transactions_for_account > table > tbody > tr:nth-child('+i+') > td:nth-child(2)')).toContainText('ONLINE TRANSFER REF #UKKSDRQG6L')
+    }
+}
--- tests/e2e/accountActivityPage.spec.ts
+++ tests/e2e/accountActivityPage.spec.ts
@@ -1,8 +1,8 @@
 import test from "@playwright/test";
 import {loadPage,closePage,clickOnOnlineBanking,loginPage} from '../../helpers/helper'
-import {clickOnShowTransaction,verifyDataInTransaction} from '../../helpers/onlineBankingHelpers/accountActivityHelper'
+import {clickOnShowTransaction,verifyDataInTransaction,clickOnFindTransaction,verifyDataInFindTransaction} from '../../helpers/onlineBankingHelpers/accountActivityHelper'
 import {clickOnAccountActivity} from '../../helpers/onlineBankingHelper'
-test.describe('Account Activity - Show Transaction',()=>{
+test.describe.parallel('Account Activity',()=>{
     test.beforeEach(async({page})=>{
         await loadPage(page)
         await loginPage(page)
@@ -13,6 +13,11 @@ test.describe('Account Activity - Show Transaction',()=>{
     test('Verify data on Show Transaction',async({page})=>{
         await clickOnShowTransaction(page)
         await verifyDataInTransaction(page)
+    })
+
+    test('Verify data on Find Transaction',async({page})=>{
+        await clickOnFindTransaction(page)
+        await verifyDataInFindTransaction(page)
     })
 
     test.afterEach(async({page})=>{
